56aa3e46c6de8002bbc051e9726e27cb79274bd0
Travis Vesbach <tav1990@hotmail.com>
2020-05-16 21:36:58 -0500

    initial working commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### Snake C# and WPF
+
+This is the classic snake game built with c# and WPF following this tutorial (https://www.wpf-tutorial.com/creating-game-snakewpf/introduction/)
+
+Feel free to download it and give it a play.  The exe file can be found in snake/bin/debug.
---
+++ snake/SnakePart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class SnakePart
+    {
+        public System.Windows.UIElement UiElement { get; set; }
+
+        public System.Windows.Point Position { get; set; }
+
+        public bool IsHead { get; set; }
+    }
+}
